38c58445a627cc878925034f32cbe6dc37655eda
RafaelCTobar <rafaelctobar569@gmail.com>
2024-02-28 21:19:09 -0500

    fix: change the adapter to vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
-import node from "@astrojs/node";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), vue()],
   output: 'hybrid',
-  adapter: node({
-    mode: 'standalone'
-  })
+  adapter: vercel()
 });
