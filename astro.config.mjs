febc48b69876030e7d112ab9ba415ff5b30e97ec
RafaelCTobar <rafaelctobar569@gmail.com>
2024-02-28 22:33:19 -0500

    a

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import vercel from '@astrojs/vercel/serverless';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), vue()],
-  output: 'hybrid',
+  output: 'server',
   adapter: vercel()
 });
