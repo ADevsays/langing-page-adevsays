eda61dfaa29d8a046987599116515d3d96058772
RafaelCTobar <rafaelctobar569@gmail.com>
2024-02-28 22:15:04 -0500

    try without server

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import vercel from '@astrojs/vercel/serverless';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), vue()],
-  output: 'server',
-  adapter: vercel()
+
 });
