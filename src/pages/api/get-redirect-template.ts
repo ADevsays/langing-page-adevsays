7109548009e957396d776f0783acce559e45299e
RafaelCTobar <rafaelctobar569@gmail.com>
2024-12-19 21:22:55 -0500

    add redirect page

--- src/pages/api/get-redirect-template.ts
+++ src/pages/api/get-redirect-template.ts
@@ -1,4 +1,5 @@
 import type { APIRoute } from "astro";
+export const prerender=false;
 
 export const GET: APIRoute = async()=>{
     const targetUrl = "https://drive.google.com/file/d/1DOdvt4lK0plTSm2OJJU-V1JWK0FTbR2F/view?usp=drive_link"
