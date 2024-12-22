aa159f898d6f019263f01fa33186df2eb25242a9
RafaelCTobar <rafaelctobar569@gmail.com>
2024-12-22 01:29:12 -0500

    change stpm to api

--- src/pages/api/post-send-nodemailer.ts
+++ src/pages/api/post-send-nodemailer.ts
@@ -6,13 +6,13 @@ export const POST: APIRoute = async ({request})=>{
     console.log("call")
     try {
         const {email, name} = await new Response(request.body).json();
-        const options = await sendEmail(email, name);
-        return new Response(JSON.stringify({success: `The email ${email} was be send`, options}),{
+        await sendEmail(email, name);
+        return new Response(JSON.stringify({success: `The email ${email} was be send`}),{
             status: 200,
             headers: { 'Content-Type': 'application/json' },
         });
     } catch (error) {
-        return new Response(JSON.stringify({error: "Have been a error with email"}),{
+        return new Response(JSON.stringify({error: `Have been a error with email ${error}`}),{
             status: 500,
             headers: { 'Content-Type': 'application/json' },
         });
--- src/services/email/sendEmail.ts
+++ src/services/email/sendEmail.ts
@@ -1,39 +1,69 @@
-import dotenv from "dotenv";
-import nodemailer from "nodemailer";
-import { htmlTemplateEmail } from "src/utils/htmlTemplateEmail";
+// import dotenv from "dotenv";
+// import nodemailer from "nodemailer";
+// import { htmlTemplateEmail } from "src/utils/htmlTemplateEmail";
+
+// const transporter = nodemailer.createTransport({
+//   host: process.env.BREVO_HOST,
+//   port: process.env.BREVO_PORT,
+//   secure: false, 
+//   auth: {
+//     user: process.env.BREVO_USER, 
+//     pass: process.env.BREVO_PASS,       
+//   },
+//   headers: {
+//     "X-Mailin-Track": "false", 
+//     "X-Mailin-TrackLinks": "false", 
+//   },
+// });
+
+// export async function sendEmail(email:string, name:string){
+//     const html = htmlTemplateEmail(name);
+//     const mailOptions = {
+//         from: '"Adevsays" oficial@adevsays.com',
+//         to: email,
+//         subject: "Compra de DevJourney",
+//         html,
+//     };
+
+//     console.log(mailOptions, process.env.BREVO_HOST)
+//     try {
+//       const info = await transporter.sendMail(mailOptions);
+//       console.log("Email enviado", info.response)
+
+//     } catch (error) {
+//       console.error("Error", error);
+//       throw error;
+//     }
+
+// }
+
+
+import brevo from '@getbrevo/brevo';
+import dotenv from 'dotenv';
+import { htmlTemplateEmail } from 'src/utils/htmlTemplateEmail';
 dotenv.config();
 
-const transporter = nodemailer.createTransport({
-  host: process.env.BREVO_HOST,
-  port: process.env.BREVO_PORT,
-  secure: false, 
-  auth: {
-    user: process.env.BREVO_USER, 
-    pass: process.env.BREVO_PASS,       
-  },
-  headers: {
-    "X-Mailin-Track": "false", 
-    "X-Mailin-TrackLinks": "false", 
-  },
-});
+console.log(brevo)
 
-export async function sendEmail(email:string, name:string){
-    const html = htmlTemplateEmail(name);
-    const mailOptions = {
-        from: '"Adevsays" oficial@adevsays.com',
-        to: email,
-        subject: "Compra de DevJourney",
-        html,
-    };
+const apiInstance = new brevo.TransactionalEmailsApi() as any;
+const apiKey = apiInstance.authentications["apiKey"];
+apiKey.apiKey = process.env.BREVO_API_KEY;
 
-    console.log(mailOptions, process.env.BREVO_HOST)
-      
-    await transporter.sendMail(mailOptions, (error:Error, info:{response:string}) => {
-        if (error) {
-            return console.error("Error:", error);
-        }
-        console.log("Correo enviado:", info.response);
-    });
-
-    return mailOptions;
+// Función para enviar un email transaccional
+export async function sendEmail(email:string, name:string) {
+    try {
+        const sendSmtpEmail = new brevo.SendSmtpEmail();
+        sendSmtpEmail.subject = "Compra de DevJourney";
+        sendSmtpEmail.htmlContent = htmlTemplateEmail(name);
+        sendSmtpEmail.sender = { "name": "Adevsays", "email": "oficial@adevsays.com" };
+        sendSmtpEmail.to = [{email, name}];
+        sendSmtpEmail.headers = {"X-Mailin-TrackLinks": "false"};
+        const response = await apiInstance.sendTransacEmail(sendSmtpEmail);
+        console.log('Email enviado con éxito', );
+        return response;
+    } catch (error) {
+        console.error('Error enviando email:', error);
+        throw error;
+    }
 }
+
